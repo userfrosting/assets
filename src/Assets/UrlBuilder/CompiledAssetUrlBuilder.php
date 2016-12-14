661090e1dd6cabc9a9d7caf4d1a41fa761a8fe10
alexweissman <alex@alexanderweissman.com>
2016-12-14 00:23:52 -0500

    oops

--- src/Assets/UrlBuilder/CompiledAssetUrlBuilder.php
+++ src/Assets/UrlBuilder/CompiledAssetUrlBuilder.php
@@ -10,25 +10,21 @@
 namespace UserFrosting\Assets\UrlBuilder;
 
 /**
- * Represents an asset bundle, as defined in https://github.com/dowjones/gulp-bundle-assets
+ * Builds a URL for a compiled asset by simply prefixing a path with a base url.
  *
- * An asset bundle can contain any number of JavascriptAssets and CssAssets, and represent either raw or compiled assets.
- * @see https://github.com/dowjones/gulp-bundle-assets.
  * @author Alex Weissman (https://alexanderweissman.com)
  */
 class CompiledAssetUrlBuilder implements AssetUrlBuilderInterface
 {
     /**
-     *  The base url for your assets, for example https://example.com/assets-raw/
-     *
-     * @param string
+     * @param string The base url for your assets, for example https://example.com/assets/
      */
     protected $baseUrl;
 
     /**
      * CompiledAssetUrlBuilder constructor.
      *
-     * @param string $baseUrl The base url to use, for example https://example.com/assets/, or http://localhost/myproject/public/assets/
+     * @param string $baseUrl
      */
     public function __construct($baseUrl)
     {
@@ -37,11 +33,12 @@ class CompiledAssetUrlBuilder implements AssetUrlBuilderInterface
 
     /**
      * Generate an absolute URL for an asset, by simply concatenating the baseUrl and the specified path.
+     *
+     * @param string $path Relative path to the desired asset
      */
     public function getAssetUrl($path)
     {
         $absoluteUrl = $this->baseUrl . $path;
-
         return $absoluteUrl;
     }
 }

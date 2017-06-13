bf2a6e5b2ef35cb4046cb120c08ce01bc4a6ad57
alexweissman <alex@alexanderweissman.com>
2017-06-13 11:39:00 -0400

    bump userfrosting/support

--- src/Assets/UrlBuilder/CompiledAssetUrlBuilder.php
+++ src/Assets/UrlBuilder/CompiledAssetUrlBuilder.php
@@ -4,7 +4,6 @@
  *
  * @package   userfrosting/assets
  * @link      https://github.com/userfrosting/assets
- * @copyright Copyright (c) 2013-2016 Alexander Weissman
  * @license   https://github.com/userfrosting/UserFrosting/blob/master/licenses/UserFrosting.md (MIT License)
  */
 namespace UserFrosting\Assets\UrlBuilder;
